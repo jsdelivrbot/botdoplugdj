1d1d9dbd1e50b1fcd7af6a8946b3aff1139bf4af
rafaell320 <rafaelinfame81@gmail.com>
2015-02-04 00:02:24 -0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -54,7 +54,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/Yemasthui/basicBot/master/lang/langIndex.json", function (json) {
+        $.get("https://raw.githubusercontent.com/rafaell320/botdoplugdj/master/lang/pt.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
